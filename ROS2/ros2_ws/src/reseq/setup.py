531cf970f90e50112d6add30bc6a0fe4978186e9
lorixhack <103076648+lorixhack@users.noreply.github.com>
2024-03-25 13:43:23 +0100

    Update setup.py

--- ROS2/ros2_ws/src/reseq/setup.py
+++ ROS2/ros2_ws/src/reseq/setup.py
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ubuntu',
-    maintainer_email='ubuntu@todo.todo',
-    description='TODO: Package description',
-    license='Apache-2.0',
+    maintainer='loriciocca',
+    maintainer_email='lorenzociocca333@gmail.com',
+    description='Temperature sensor/logger',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
