0cbeefedbb9aa2ddb9a8e3ec80540920b77b4840
Salvatore Calo <dafder45678@gmail.com>
2025-09-25 18:57:29 +0200

    fest(sidebar): created sidebar component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import type { SidebarMenu } from "./logic/list";
+
+function App() {
+    const [sidebarMenuTask, setSidebarMenuTask] = useState<SidebarMenu[]>();
+    
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/logic/list.ts
@@ -0,0 +1,29 @@
+export interface SidebarMenu {
+    icon: string,
+    text: string,
+}
+
+/*
+    sidebarMenuTasks = [
+        {
+            icon: Home,
+            text: Home,
+            tasks: [
+                {
+                    "Name",
+                    "isCompleted"
+                }
+            ]
+        },
+        {
+            icon: Second,
+            text: Second,
+            tasks: [
+                {
+                    "Name",
+                    "isCompleted"
+                }
+            ]
+        }
+    ]
+*/
